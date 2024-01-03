3f76e459d258aed90d41c14674ccf6982198c7d2
AntonFedoseev <fedoseev446@gmail.com>
2024-01-03 16:41:45 +0300

    Settings log4j and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,17 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.seleniumhq.selenium:selenium-java:4.15.0")
+    implementation("org.seleniumhq.selenium:selenium-chrome-driver:4.15.0")
+    implementation("org.seleniumhq.selenium:selenium-devtools-v120:4.16.0")
+
+    testImplementation("org.testng:testng:7.8.0")
+
+    implementation("org.slf4j:slf4j-api:2.0.9")
+    testImplementation("org.slf4j:slf4j-reload4j:2.0.9")
+    implementation("org.apache.logging.log4j:log4j-core:2.22.0")
 }
 
 tasks.test {
-    useJUnitPlatform()
+    useTestNG()
 }
